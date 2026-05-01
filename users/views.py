320c6314379cad7087c6889a4ef6f9edce5f8942
Dominic-Bravo <dominicianbravo017@gmail.com>
2026-05-01 20:33:11 +0800

    fix: views bug

--- users/views.py
+++ users/views.py
@@ -18,3 +18,5 @@ class GoogleLogin(SocialLoginView):
 
 class FacebookLogin(SocialLoginView):
     adapter_class = FacebookOAuth2Adapter
+    callback_url = "http://localhost:3000/facebook-callback"
+    client_class = OAuth2Client  # This fixes the current error
